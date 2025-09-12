da91269b88853382d0f38458844f26dd5f33d1de
unknown <yurii.lisovenko@bazucompany.com>
2025-09-12 18:07:56 +0300

    burger fixes

--- app/layouts/header-footer.vue
+++ app/layouts/header-footer.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const isOpenedNavBarLinks = ref(false);
+</script>
+
 <template>
   <nav class="navbar">
     <div class="padding-global">
@@ -6,14 +10,40 @@
           <NuxtLink to="/" class="navbar_logo-wrap w-inline-block">
             Anastasiia Yablonska
           </NuxtLink>
-          <div class="navbar_links" style="">
-            <NuxtLink class="navbar_link" to="/">Works</NuxtLink>
-            <NuxtLink class="navbar_link" to="/about">About</NuxtLink>
-            <NuxtLink class="navbar_link" to="/gallery">Gallery</NuxtLink>
-            <NuxtLink class="navbar_link" to="/contacts">Contacts</NuxtLink>
+          <div class="navbar_links" :class="{ active: isOpenedNavBarLinks }">
+            <NuxtLink
+              to="/"
+              class="navbar_link"
+              @click.stop="isOpenedNavBarLinks = false"
+              >Works</NuxtLink
+            >
+            <NuxtLink
+              class="navbar_link"
+              @click.stop="isOpenedNavBarLinks = false"
+              to="/about"
+              >About</NuxtLink
+            >
+            <NuxtLink
+              class="navbar_link"
+              @click.stop="isOpenedNavBarLinks = false"
+              to="/gallery"
+              >Gallery</NuxtLink
+            >
+            <NuxtLink
+              class="navbar_link"
+              @click.stop="isOpenedNavBarLinks = false"
+              to="/contacts"
+              >Contacts</NuxtLink
+            >
           </div>
-          <div class="navbar_hamburger-wrap">
-            <div class="navbar_hamburger">
+          <div
+            class="navbar_hamburger-wrap"
+            @click="isOpenedNavBarLinks = !isOpenedNavBarLinks"
+          >
+            <div
+              class="navbar_hamburger"
+              :class="{ active: isOpenedNavBarLinks }"
+            >
               <div class="navbar_hamburger-line _1" style=""></div>
               <div class="navbar_hamburger-line _2" style=""></div>
             </div>
@@ -29,4 +59,22 @@
     <p>&copy; 2023 Anastasia Yablonska. All rights reserved.</p>
   </footer>
 </template>
-<script setup lang="ts"></script>
+<style lang="css" scoped>
+.mob {
+  display: none;
+}
+@media screen and (max-width: 479px) {
+  .mob {
+    display: flex;
+  }
+}
+
+.navbar_hamburger ._1 {
+  transition: all 0.3s ease;
+}
+.navbar_hamburger.active ._1 {
+  width: 0;
+  height: 0;
+  opacity: 0;
+}
+</style>
